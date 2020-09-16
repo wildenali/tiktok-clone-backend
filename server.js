36c513e4359039fcfab7e45ad33b57351d7ee159
wildenali <wildenwildenaliali@gmail.com>
2020-09-16 22:59:48 +0700

    Kasih comment untuk apa yg akan dilakukan

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 
+// app config
 const app = express();
 const port = 9000;
 
+// middlewares
+
+// DB Congig
+
+// api endpoints
 app.get("/", (req, res) => res.status(200).send("Hellow coy"));
 
+// listen
 app.listen(port, () => console.log(`Listening on localhost:${port}`));
