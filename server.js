f556f3ab20849998e0465e21145e51ef7873210c
wildenali <wildenwildenaliali@gmail.com>
2020-09-17 17:36:56 +0700

    fix setHeader

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const port = process.env.PORT || 9000;
 // middlewares
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeaders("Access-Control-Allow-Origin", "*"),
-    res.setHeaders("Access-Control-Allow-Headers", "*"),
+  res.setHeader("Access-Control-Allow-Origin", "*"),
+    res.setHeader("Access-Control-Allow-Headers", "*"),
     next();
 });
 
